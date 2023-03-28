303fbe69615ea7919506d6ef966f491d7319daca
shreyansh-k <118434205+shreyansh-k@users.noreply.github.com>
2023-03-29 00:10:10 +0530

    Add files via upload

---
+++ criss_workshop.py
@@ -0,0 +1,15 @@
+import socket
+
+serverIP = '172.17.29.11'
+serverPORT = 6000
+
+serveraddress = (serverIP,serverPORT)
+bufferSize = 1024
+
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)#DGRAM: User Data ...
+
+message= ''
+
+
+bytestosend = str.encode(message)
+UDPClientSocket.sendto(bytestosend, serveraddress)
